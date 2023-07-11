add9db558c1448bd61d6eaa49d5ce4cc0c442f17
mammamon <quangnguyenonlinevn@gmail.com>
2023-07-12 03:02:39 +0700

    update

---
+++ models/models.js
@@ -0,0 +1,17 @@
+// NavPill class
+class NavPill {
+  constructor(showName, type) {
+    this.showName = showName;
+    this.type = type;
+  }
+}
+
+// Product class
+class Product {
+  constructor(type, name, imgShow, imgDress) {
+    this.type = type;
+    this.name = name;
+    this.imgShow = imgShow;
+    this.imgDress = imgDress;
+  }
+}
